2308c0475848897df517e33e5e2d3c9854339dea
Drake Axelrod <drake@draxel.io>
2022-01-21 20:12:10 +0000

    Added Social Media Buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,85 @@
-import { Box } from "@chakra-ui/react";
+import { Box, ButtonGroup, Flex, Heading, Icon, IconButton, Spacer } from "@chakra-ui/react";
 import { FC } from "react";
+import {
+  FaTwitch,
+  FaDiscord,
+  FaYoutube,
+  FaTwitter,
+  FaTiktok,
+  FaCrown,
+  FaInstagram,
+} from "react-icons/fa";
 
-type HeaderProps = {
+const socials = [
+  {
+    name: "twitch",
+    link: "https://www.twitch.tv/abookishreader",
+    icon: FaTwitch,
+  },
+  {
+    name: "youtube",
+    link: "https://www.youtube.com/channel/UCHLNGqubi6GGYsTMS8uG1tg",
+    icon: FaYoutube,
+  },
+  {
+    name: "discord",
+    link: "https://discord.gg/bfb8kS3",
+    icon: FaDiscord,
+  },
+  {
+    name: "twitter",
+    link: "https://twitter.com/abookishreader",
+    icon: FaTwitter,
+  },
+  {
+    name: "tiktok",
+    link: "https://www.tiktok.com/@abookishreader",
+    icon: FaTiktok,
+  },
+  {
+    name: "instagram",
+    link: "https://instagram.com/abookishreader_",
+    icon: FaInstagram,
+  },
+  {
+    name: "throne",
+    link: "https://jointhrone.com/u/abookishreader",
+    icon: FaCrown,
+  },
+];
+const Icons: FC = () => {
+  return (
+    <ButtonGroup>
+      {socials.map((entry: any, i: number) => {
+        return (
+          <IconButton
+            key={i}
+            icon={<Icon as={entry.icon} />}
+            href={entry.link}
+            aria-label={entry.name}
+            rel="noopener noreferrer"
+            target="_blank"
+          />
+        );
+      })}
+    </ButtonGroup>
+  );
 };
-
+const Logo: FC = () => {
+  return (
+    <Heading as="h1">
+      Bookish
+    </Heading>
+  )
+}
+type HeaderProps = {};
 const Header: FC<HeaderProps> = () => {
   return (
-    <Box as="footer">
-      Header
-    </Box>
+    <Flex direction="row" as="header">
+      <Logo />
+      <Spacer />
+      <Icons />
+    </Flex>
   );
 };
 
